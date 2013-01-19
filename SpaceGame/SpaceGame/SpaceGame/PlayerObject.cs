5a11fa60c1ece0447319f9b2feda76123f29ee2c
Callum Lawson <lawsonathome@gmail.com>
2013-01-19 23:30:56 +0000

    Player speed and animation!

--- SpaceGame/SpaceGame/SpaceGame/PlayerObject.cs
+++ SpaceGame/SpaceGame/SpaceGame/PlayerObject.cs
@@ -10,7 +10,8 @@ namespace SpaceGame
 {
     class PlayerObject:MovingWorldObject
     {
-        
+        private float speedMulti = 0.8f;
+
         public PlayerObject()
         {
 
@@ -19,27 +20,34 @@ namespace SpaceGame
         {
             Console.WriteLine(this.hitBlocks);
 
-            
-
             KeyboardState ks = Keyboard.GetState();
             if (ks.IsKeyDown(Keys.W) || ks.IsKeyDown(Keys.Up))
             {
-                this.velocity.Y -= 1;
+                this.velocity.Y -= 1 * speedMulti;
             }
             if (ks.IsKeyDown(Keys.A) || ks.IsKeyDown(Keys.Left))
             {
-                this.velocity.X -= 1;
+                this.velocity.X -= 1 * speedMulti;
             }
             if (ks.IsKeyDown(Keys.S) || ks.IsKeyDown(Keys.Down))
             {
-                this.velocity.Y += 1;
+                this.velocity.Y += 1 * speedMulti;
             }
             if (ks.IsKeyDown(Keys.D) || ks.IsKeyDown(Keys.Right))
             {
-                this.velocity.X += 1;
+                this.velocity.X += 1 * speedMulti;
             }
 
             this.angle =  (float)Math.Atan2(this.velocity.Y, this.velocity.X);
+
+            if (velocity.Length() > 0.8f)
+            {
+                this.texture.setAnim("walking");
+            }
+            else
+            {
+                this.texture.setAnim("stationary");
+            }
 
             base.Update(gameTime);
         }
@@ -48,12 +56,12 @@ namespace SpaceGame
         {
             base.Init(content);
             List<animationSub> subs= new List<animationSub>();
-            subs.Add(new animationSub("stationary",0,1));
+            subs.Add(new animationSub("stationary",0,0));
             subs.Add(new animationSub("walking",1,2));
             this.texture = new AnimatedTexture2D(content.Load<Texture2D>("player"),64,subs);
             this.collider = new CircleCollider(new Vector2(32, 32), 30);
-            this.texture.skipC = 5;
-            this.texture.setAnim("walking");
+            this.texture.skipC = 10;
+            this.texture.setAnim("stationary");
 
 
         }
